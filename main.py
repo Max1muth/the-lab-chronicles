25c87c05e4463a9e160bcf1bc0083495234b71f8
Max1muth <122169538+Max1muth@users.noreply.github.com>
2025-05-24 16:10:59 +0300

    ver_0.0

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ print(rn.randint(1, 15))  # выпало 2 => задание:
 
 note = """
 Ash nazg durbatulûk,
-
 Ash nazg gimbatul,
 Ash nazg thrakatulûk,
 Agh burzum-ishi krimpatul.
@@ -15,31 +14,22 @@ Agh burzum-ishi krimpatul.
 
 
 def shuffler(text: str):
-    n_text = ""
-    l1st = text.split(" ")  # разделяет текст на список потенциальных слов
-    for char1 in l1st:  # перебор по таких слов по списку
-        if "\n" in char1:
-            position = [i for i, n in enumerate(char1) if n == "\n"]  # список индексов встречи \n
-            print(position)
-            l2st = char1.split("\n")
-            l2st = [x for x in l2st if x != ""]  # удаление лишних ""
-            char2 = ""
-            char3 = ""
-            for i in range(len(position)):
-                print(l2st, 99999999)
-                char2 += (l2st[i])
-                print(char2, 66666)
-                for k in range(len(char2)-1, -1, -1):
-                    char3 += char2[k]
-                print(char3, 77777)
-            n_text += "\n" + char3 + " "
+    n_text = [""]
+    w0rd = ""
+    for char in text:
+        if char.isalpha():
+            w0rd += char
         else:
-            n_text += char1
-        # while char1.index("\n") != ValueError:
-        #     print(char1.find("\n"))
-        #     print(char1[:int(char1.find("\n"))] + char1[int(char1.find("\n")):])
-    print(n_text)
-    return l1st
+            if w0rd:
+                n_text.append(w0rd[::-1])
+                w0rd = ""
+            n_text.append(char)
+    if w0rd:
+        n_text.append(w0rd[::-1])
+    return "".join(n_text)
 
 
-print(shuffler(note))
+coded = shuffler(note)
+print(coded)
+encoded = shuffler(coded)
+print(encoded)
