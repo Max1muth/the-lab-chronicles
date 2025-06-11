29782b9e07fe0c8868dd2420bce484deeca8f9b3
Max1muth <122169538+Max1muth@users.noreply.github.com>
2025-06-12 02:12:27 +0300

    beta

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
+import random as rn
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model
-import openpyxl
 
-df = pd.read_excel("датасет-1.xlsx")
-# print(df)
-pred = pd.read_excel("prediction_price.xlsx")
-# print(pred)
+rn.seed(521)
+tasks = [rn.randint(1, 11), rn.randint(1, 11), rn.randint(1, 11)] # 2, 1, 5
+print(tasks)
 
-plt.scatter(df.area, df.price, color='red')
-plt.xlabel("площадь(м**2)")
-plt.ylabel("стоимость(млн.руб)")
+df = pd.read_csv("data.csv")
+j = df.iloc[0, 0]
+print(eval(j)[1])
+x11, y11 = [], []
+x12, y12 = [], []
+x13, y13 = [], []
+for i in range(len(df.iloc[0])):
+    j1, j2, j3 = df.iloc[0, i], df.iloc[1, i], df.iloc[2, i]
+    x11 += [eval(j1)[0]]
+    y11 += [eval(j1)[1]]
+    x12 += [eval(j2)[0]]
+    y12 += [eval(j2)[1]]
+    x13 += [eval(j3)[0]]
+    y13 += [eval(j3)[1]]
+print(x11, y11)
+
+plt.scatter(x11, y11, color="red")
+plt.scatter(x12, y12, color="blue")
+plt.scatter(x13, y13, color="green")
 plt.show()
-
-reg = linear_model.LinearRegression()
-reg.fit(df[['area']], df.price)
-
-# print(reg.predict([[38]])) # [3.52747127]
-
-price = reg.coef_ * df.area + reg.intercept_
-
-plt.scatter(df.area, df.price, color='red')
-plt.plot(df.area, reg.predict(df[['area']]))
-plt.xlabel("площадь(м**2)")
-plt.ylabel("стоимость(млн.руб)")
-plt.show()
-
-p = reg.predict(pred)
-
-pred["predicted_prices"] = p
-pred.to_excel("new.xlsx", index=False)
-
-print(pred)
